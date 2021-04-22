c5d86bffd1c9f8ac524043c082f33e5698a662f9
Chris <christopher_brisson@hotmail.com>
2021-04-21 23:43:14 -0300

    Fixing bad import

--- src/components/price_targets/new_price_target.js
+++ src/components/price_targets/new_price_target.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
-
-import Button from "@material-ui/core/button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogTitle from "@material-ui/core/DialogTitle";
-
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@material-ui/core";
 import NewPriceTargetFields from "./new_price_target_fields";
-
 import { createPriceTarget } from "../../actions/price_targets.js";
 
 const NewPriceTarget = ({ updateListCallback }) => {
